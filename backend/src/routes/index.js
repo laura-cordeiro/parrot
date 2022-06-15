1f3446e7e51e4b2cf1ddaa6fc64e0321debfbd1d
Danilo Sales <danilo.pereira.sales@gmail.com>
2022-06-15 00:20:45 -0300

    Inclusão das rotas de Posts

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,20 @@
 const express = require("express"),
   routes = express.Router(),
-  UsersController = require("../domain/users/controllers/usersController");
+  UsersController = require("../domain/users/controllers/usersController"),
+  PostsController = require("../domain/posts/controllers/postsController");
+
 
 routes.post("/users", UsersController.createUser);
 routes.get("/users", UsersController.readUsers);
 routes.put("/users/:id", UsersController.updateUsers);
 routes.delete("/users/:id", UsersController.deleteUsers);
+
+
+routes.get("/user-posts/:id",PostsController.getUserPosts);
+routes.get("/all-posts",PostsController.getAllPosts);
+routes.post("/post",PostsController.createPost);
+
+
+
 
 module.exports = routes;
