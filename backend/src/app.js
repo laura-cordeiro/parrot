7c53a5895004d998822ec67a74665300a66bbec6
Vi Landim <landim.united@hotmail.com>
2022-06-16 15:53:22 -0300

    Mudança da porta do servidor de back, pois estava dando conflito com o servidor de Front

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,8 @@ const express = require("express"),
 
 db.hasConnection();
 app.use(express.json());
-app.use(routes);
 app.use(cors());
+app.use(routes);
 
-app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+
+app.listen(4000, () => console.log("Servidor rodando na porta 3000"));
