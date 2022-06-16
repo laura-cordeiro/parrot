b06e3c5627d36419875f007f51394294d69a4679
Danilo Sales <danilo.pereira.sales@gmail.com>
2022-06-16 12:58:51 -0300

    Retirado ID do Post na criação do mesmo

--- backend/src/domain/posts/validations/createPost.js
+++ backend/src/domain/posts/validations/createPost.js
@@ -2,7 +2,6 @@ const { validate, Joi } = require("express-validation");
 
 module.exports = validate({
   body: Joi.object({
-    id: Joi.number().required().integer(),
     idUser: Joi.number().required().integer(),
     content: Joi.string().required().max(200)
   }),
