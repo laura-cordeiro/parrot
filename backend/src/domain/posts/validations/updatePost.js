639b87bad5d74c64e63256da2cb2536447dcdd44
Danilo Sales <danilo.pereira.sales@gmail.com>
2022-06-16 12:54:14 -0300

    Correção no método de validação do parametro

--- backend/src/domain/posts/validations/updatePost.js
+++ backend/src/domain/posts/validations/updatePost.js
@@ -2,7 +2,7 @@ const { validate, Joi } = require("express-validation");
 
 module.exports = validate({
   params: Joi.object({
-    id: Joi.number().required().integer,
+    id: Joi.number().required().integer(),
   }),
   body: Joi.object({
     content: Joi.string().max(200).required(),
