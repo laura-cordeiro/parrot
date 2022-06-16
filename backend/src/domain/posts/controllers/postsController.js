a57137df1be18746035b5fe6162098e9b379f5d5
Danilo Sales <danilo.pereira.sales@gmail.com>
2022-06-16 01:25:34 -0300

    Adicionado os métodos crud

--- backend/src/domain/posts/controllers/postsController.js
+++ backend/src/domain/posts/controllers/postsController.js
@@ -1,4 +1,5 @@
 const { Posts } = require("../models");
+const { Users } = require("../../users/models");
 
 const PostsController = {
   async getAllPosts(req, res) {
@@ -6,19 +7,68 @@ const PostsController = {
       const { page = 1 } = req.query;
       const limit = 10;
       const offset = limit * (parseInt(page) - 1);
+
       let filter = {
         limit,
-        offset
+        offset,
       };
       const posts = await Posts.findAll(filter);
+
+      if (!posts.length) {
+        return res.status(404).json("Eita! Não foi feito nenhum post ainda");
+      }
+
       return res.status(200).json(posts);
     } catch (error) {
-      return res.status(500).json("Erro ao listar os posts");
+      return res.status(500).json("Erro não catalogado!");
+    } 
+  },
+  async getUserPosts(req, res) {
+    try {
+      const { id } = req.params;
+      const { page = 1 } = req.query;
+      const limit = 10;
+      const offset = limit * (parseInt(page) - 1);
+
+      let filter = {};
+
+      if (id) {
+        Object.assign(filter, {
+          where: {
+            idUser: id,
+          },
+          limit,
+          offset,
+        });
+      }
+
+      const posts = await Posts.findAll(filter);
+
+      if (!posts.length) {
+        return res.status(404).json("Id não encontrado!");
+      }
+      res.status(200).json(posts);
+    } catch (error) {
+      res.status(500).json("Erro não catalogado! ");
     }
   },
-  getUserPosts(req, res) {},
 
-  createPost(req, res) {}
+  async createPost(req, res) {
+    try {
+      const {idUser} = req.body
+      const checkUser = await Users.count({ where: { idUser } });
+      if (!checkUser) {
+        return res.status(400).send("Id não encontrado!");
+      }
+      console.log(req.body)
+      const newPost = await Posts.create({
+        ...req.body,
+      });
+      return res.status(201).json(newPost);
+    } catch (error) {
+      res.status(500).json("Erro não catalogado!");
+    }
+  },
 };
 
 module.exports = PostsController;
