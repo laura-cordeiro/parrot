f812697644c693ba9958ce0479581e108d32aca7
Danilo Sales <danilo.pereira.sales@gmail.com>
2022-06-16 12:51:43 -0300

    Atualizado para utilizar conceito de singularidade

--- backend/src/domain/posts/controllers/postsController.js
+++ backend/src/domain/posts/controllers/postsController.js
@@ -1,5 +1,6 @@
 const { Posts } = require("../models");
 const { Users } = require("../../users/models");
+const PostsServices = require("../services/postsServices");
 
 const PostsController = {
   async getAllPosts(req, res) {
@@ -21,7 +22,7 @@ const PostsController = {
       return res.status(200).json(posts);
     } catch (error) {
       return res.status(500).json("Erro não catalogado!");
-    } 
+    }
   },
   async getUserPosts(req, res) {
     try {
@@ -55,18 +56,59 @@ const PostsController = {
 
   async createPost(req, res) {
     try {
-      const {idUser} = req.body
-      const checkUser = await Users.count({ where: { idUser } });
+      const { idUser } = req.body;
+      const checkUser = await PostsServices.findUser(idUser);
       if (!checkUser) {
         return res.status(400).send("Id não encontrado!");
       }
-      console.log(req.body)
-      const newPost = await Posts.create({
-        ...req.body,
-      });
+      const newPost = await PostsServices.createPost(req.body);
+      if (!newPost) {
+        return res.status(500).json("Falha na criação do post!");
+      }
       return res.status(201).json(newPost);
     } catch (error) {
       res.status(500).json("Erro não catalogado!");
+    }
+  },
+
+  async updatePost(req, res) {
+    try {
+      const { id } = req.params;
+
+      const checkPost = await PostsServices.findPost(id);
+      if (!checkPost) {
+        return res.status(400).json("Post não encontrado!");
+      }
+
+      const updatePost = await PostsServices.updatePost(id, req.body);
+      return res.status(200).json("Post atualizado!");
+    } catch (error) {
+      res
+        .status(400)
+        .json("Erro não catalogado!");
+    }
+  },
+  async deletePost(req, res) {
+    try {
+      
+      const { id } = req.params;
+
+      const checkPost = await PostsServices.findPost(id);
+
+      if (!checkPost) {
+        return res.status(400).send("Post não encontrado!");
+      }
+      
+      const deletedPost = await PostsServices.deletePost(id);
+      return res.status(204).json("Post excluido com sucesso!")
+
+    } catch (error) {
+      console.log(error);
+      res
+        .status(400)
+        .json(
+          "Erro não catalogado!"
+        );
     }
   },
 };
