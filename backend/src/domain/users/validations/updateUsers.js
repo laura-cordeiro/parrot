58f3a63e29068cc7cc0d4979bc19d0434e9e76b4
Danilo Sales <danilo.pereira.sales@gmail.com>
2022-06-17 10:20:01 -0300

    Alteração no validations

--- backend/src/domain/users/validations/updateUsers.js
+++ backend/src/domain/users/validations/updateUsers.js
@@ -6,7 +6,6 @@ module.exports = validate({
   }),
   body: Joi.object({
     name: Joi.string().required(),
-    email: Joi.string().email().required(),
     apartment: Joi.number().integer(),
     password: Joi.string().min(6).required()
   })
